25f65f7b290e280b9b1c2f13d2fbbfa65240f89e
Michael Modaff <Michael Modaff>
2017-02-05 20:47:01 -0600

    Base working files that cover up to the end of the first tutorial video

---
+++ Tomoko/MyBot.cs
@@ -0,0 +1,47 @@
+﻿using Discord;
+using Discord.Commands;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomokoBot
+{
+    class MyBot
+    {
+        DiscordClient discord;
+        public MyBot()
+        {
+            discord = new DiscordClient(x =>
+            {
+               x.LogLevel = LogSeverity.Info;
+               x.LogHandler = Log;
+            });
+
+            discord.UsingCommands(x =>
+            {
+                x.PrefixChar = '!';
+                x.AllowMentionPrefix = true;
+            });
+
+            var commands = discord.GetService<CommandService>();
+
+            commands.CreateCommand("hello")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendMessage("Hi!");
+                });
+
+            discord.ExecuteAndWait(async () =>
+            {
+                await discord.Connect("MjcxNzU5MjcyNDU3NTM1NDg5.C27mZw.dcXLPH1pul-t4TK1QUa4qQr1DdI", TokenType.Bot);
+            });
+        }
+        public void Log(object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
